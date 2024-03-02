377fe7e919c4bc8de3e06ddaf6d562e8543dcb57
Arulmozhikumar <arulmozhikumar7@gmail.com>
2024-03-02 07:03:22 +0530

    Added getallPizzas

--- controllers/pizzaController.js
+++ controllers/pizzaController.js
@@ -16,3 +16,15 @@ exports.addpizza = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+exports.getallPizza = async (req, res) => {
+  try {
+    const pizzas = await Pizza.find().populate({
+      path: "ingredients",
+      select: "name",
+    });
+    res.status(200).json(pizzas);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- routes/pizzaRoutes.js
+++ routes/pizzaRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const pizzaController = require("../controllers/pizzaController");
 
 router.post("/addpizza", pizzaController.addpizza);
-
+router.get("/getpizzas", pizzaController.getallPizza);
 module.exports = router;
