77d3fc1dec9705ef392ad8eca9431d5b66ff6a7f
Arulmozhikumar <arulmozhikumar7@gmail.com>
2024-03-10 23:24:18 +0530

    Updated CORS

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors");
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "pizza-delivery-application-frontend.vercel.app",
   })
 );
 const routes = require("./routes");
